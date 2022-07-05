b434f85e728cabd4a520ff204986984bec1c0b55
BIack1st <steveminecrap69@gmail.com>
2022-07-05 12:33:17 -0500

    added h2

--- src/Components/AvailabilityPage.tsx
+++ src/Components/AvailabilityPage.tsx
@@ -1,4 +1,7 @@
-
 export function AvailabilityPage() {
-    
+    return (
+        <div className="pt-10 ">
+            <h2 className="font-semibold text-4xl   text-center text-black">Availability</h2>
+        </div>
+    )
 }
