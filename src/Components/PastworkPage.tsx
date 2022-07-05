48d3f6ea7aab8c7d494f3d927602b9af4d67bdff
BIack1st <steveminecrap69@gmail.com>
2022-07-05 12:29:40 -0500

    indentation

--- src/Components/PastworkPage.tsx
+++ src/Components/PastworkPage.tsx
@@ -4,7 +4,7 @@ function PastworkPage() {
     return (
         <div className="pt-10 ">
             <div className="w-3/4 m-auto items-center h-3/4" >
-            <h2 className="font-semibold text-4xl  text-center text-black">Past Work</h2> 
+                <h2 className="font-semibold text-4xl  text-center text-black">Past Work</h2> 
                 <p className="text-center font-normal pt-[10px] text-lg items-center ">
                     This section will showcase my past work:
                 </p>
